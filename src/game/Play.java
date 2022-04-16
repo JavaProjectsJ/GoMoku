b3cc413e33dbc0681db6c75d0d457d3fe1607eb6
jperfor849 <jperfor849@g.educaand.es>
2022-04-16 21:38:09 +0200

    GoMoku: Add Validation for win the game
    Signed-off-by: jperfor849 <jperfor849@g.educaand.es>
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>

--- src/game/Play.java
+++ src/game/Play.java
@@ -74,13 +74,57 @@ public class Play {
 		ShowResult();
 	}
 
-	public static void validate(int[] position) {
-		/* Validate if position is in the board size 
-		 * and if the position is empty */
-		if (position[0] < 0 || position[0] > board.getTableSize || position[1] < 0 || position[1] > board.getTableSize) {
-			System.out.println("The position is not valid");
+	public static void validate(int[] position, Board board){			 
+		/* Validate if position is in the board size */
+		if (position[0] < 0 || position[0] > 14 || position[1] < 0 || position[1] > 14) {
+			System.out.println("The position is not in the board size");
+		/* Validate if the position is empty */	
 		} else if (board.getPiece(position[0], position[1]) != 0) {
-			System.out.println("The position is not empty");
+			System.out.println("The position is not empty");	
+	}
+
+	public static void validateHorizontalToWin(Board board) {
+		/* Validate if in the horizontal line are 5 pieces */
+		boolean win = false;
+		for (int i = 0; i < board.getTableSize(); i++) {
+			for (int j = 0; j < board.getTableSize() - 4; j++) {
+				if (board.getPiece(i, j) == board.getPiece(i, j + 1)
+						&& board.getPiece(i, j) == board.getPiece(i, j + 2)
+						&& board.getPiece(i, j) == board.getPiece(i, j + 3)
+						&& board.getPiece(i, j) == board.getPiece(i, j + 4)) {
+					win = true;
+				}
+			}
+		}
+	}
+	
+	public static void validateVerticalToWin(Board board) {
+		/* Validate if in the vertical line are 5 pieces */
+		boolean win = false;
+		for (int i = 0; i < board.getTableSize() - 4; i++) {
+			for (int j = 0; j < board.getTableSize(); j++) {
+				if (board.getPiece(j, i) == board.getPiece(j + 1, i)
+						&& board.getPiece(j, i) == board.getPiece(j + 2, i)
+						&& board.getPiece(j, i) == board.getPiece(j + 3, i)
+						&& board.getPiece(j, i) == board.getPiece(j + 4, i)) {
+					win = true;
+				}
+			}
+		}
+	}
+
+	public static void validateDiagonalToWin(Board board) {
+		/* Validate if in the diagonal line are 5 pieces */
+		boolean win = false;
+		for (int i = 0; i < board.getTableSize() -4; i++) {
+			for (int j = 0; j < board.getTableSize() - 4; j++) {
+				if (board.getPiece(i, j) == board.getPiece(i + 1, j + 1)
+						&& board.getPiece(i, j) == board.getPiece(i + 2, j + 2)
+						&& board.getPiece(i, j) == board.getPiece(i + 3, j + 3)
+						&& board.getPiece(i, j) == board.getPiece(i + 4, j + 4)) {
+					win = true;
+				}
+			}
 		}
 	}
 
