8b7e7e3f42ab877b513a2edc5cf6b74586523de3
jperfor849 <jperfor849@g.educaand.es>
2022-04-18 20:57:46 +0200

    GoMoku: Add more names
     * Names are taken from: https://www.top500.org/lists/top500/list/2021/11/
    
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>
    Signed-off-by: jperfor849 <jperfor849@g.educaand.es>

--- src/game/Database.java
+++ src/game/Database.java
@@ -3,7 +3,40 @@ package game;
 import java.util.Random;
 
 public enum Database {
-	NAME(new String[] { "Kasparov", "S1mple", "TeNSe198_v2" });
+	// Create the 100 name of supercomputers for the players
+	NAME(new String[] {
+			"MareNostrum",
+			"AlphaGo",
+			"DeepMind",
+			"AlphaZero",
+			"AlphaStar",
+			"Watson",
+			"DeepThought",
+			"Fugaku",
+			"Summit",
+			"Sierra",
+			"Sunway Taihu",
+			"Perimutter",
+			"Selene",
+			"Voyager-EUS2",
+			"HPC5",
+			"Juwels",
+			"CEA-HF",
+			"Frontera",
+			"Polaris",
+			"ABCI2.0",
+			"Chervonenkis",
+			"Piz Daint",
+			"Trinity",
+			"ARCHER 2",
+			"Hawk"
+			"Lassen",
+			"Guru",
+			"Maru",
+			"PANGEA III",
+			"HiperGator AI"
+			"Cactus"
+	});
 
 	private String[] name;
 
