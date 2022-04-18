13d43a2b67e45602dc7b0a195d91347e866e48b6
jperfor849 <jperfor849@g.educaand.es>
2022-04-18 21:40:34 +0200

    GoMoku: Database: Add some missing ,
    Signed-off-by: jperfor849 <jperfor849@g.educaand.es>
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>

--- src/game/Database.java
+++ src/game/Database.java
@@ -4,39 +4,10 @@ import java.util.Random;
 
 public enum Database {
 	// Create the 100 name of supercomputers for the players
-	NAME(new String[] {
-			"MareNostrum",
-			"AlphaGo",
-			"DeepMind",
-			"AlphaZero",
-			"AlphaStar",
-			"Watson",
-			"DeepThought",
-			"Fugaku",
-			"Summit",
-			"Sierra",
-			"Sunway Taihu",
-			"Perimutter",
-			"Selene",
-			"Voyager-EUS2",
-			"HPC5",
-			"Juwels",
-			"CEA-HF",
-			"Frontera",
-			"Polaris",
-			"ABCI2.0",
-			"Chervonenkis",
-			"Piz Daint",
-			"Trinity",
-			"ARCHER 2",
-			"Hawk"
-			"Lassen",
-			"Guru",
-			"Maru",
-			"PANGEA III",
-			"HiperGator AI"
-			"Cactus"
-	});
+	NAME(new String[] { "MareNostrum", "AlphaGo", "DeepMind", "AlphaZero", "AlphaStar", "Watson", "DeepThought",
+			"Fugaku", "Summit", "Sierra", "Sunway Taihu", "Perimutter", "Selene", "Voyager-EUS2", "HPC5", "Juwels",
+			"CEA-HF", "Frontera", "Polaris", "ABCI2.0", "Chervonenkis", "Piz Daint", "Trinity", "ARCHER 2", "Hawk",
+			"Lassen", "Guru", "Maru", "PANGEA III", "HiperGator AI", "Cactus" });
 
 	private String[] name;
 
