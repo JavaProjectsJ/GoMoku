046189659fb56b2b55b3aaddfb18cb81bd7236fd
jperfor849 <jperfor849@g.educaand.es>
2022-04-22 10:57:36 +0200

    GoMoku: KeyboardGame: Reset the keyboard after any read
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>
    Signed-off-by: jperfor849 <jperfor849@g.educaand.es>

--- src/game/KeyboardGame.java
+++ src/game/KeyboardGame.java
@@ -5,16 +5,22 @@ import java.util.Scanner;
 public class KeyboardGame {
 	Scanner keyboard = new Scanner(System.in);
 
+	public void reset() {
+		keyboard.reset();
+	}
+
 	public boolean readChar(char affirmativeValue) {
 		char c = keyboard.next().charAt(0);
 		if (affirmativeValue == c) {
 			return true;
 		}
+		reset();
 		return false;
 	}
 
 	public int readInt() {
 		int i = keyboard.nextInt();
+		reset();
 		return i;
 	}
 }
