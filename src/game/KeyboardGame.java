68a00d8789a97a875394c4e80ee3151026e10f11
jperfor849 <jperfor849@g.educaand.es>
2022-04-22 10:56:13 +0200

    GoMoku: KeyboardGame: Drop an unneeded SuppressWarning
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>
    Signed-off-by: jperfor849 <jperfor849@g.educaand.es>

--- src/game/KeyboardGame.java
+++ src/game/KeyboardGame.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class KeyboardGame {
 	Scanner keyboard = new Scanner(System.in);
 
-	@SuppressWarnings("resource")
 	public boolean readChar(char affirmativeValue) {
 		char c = keyboard.next().charAt(0);
 		if (affirmativeValue == c) {
