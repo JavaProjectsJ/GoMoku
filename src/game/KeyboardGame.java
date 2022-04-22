316eb60b3afad842bfd026440a29ac0e4ee47525
jperfor849 <jperfor849@g.educaand.es>
2022-04-22 10:42:38 +0200

    GoMoku: KeyboardGame: Make the keyboard being global
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>
    Signed-off-by: jperfor849 <jperfor849@g.educaand.es>

--- src/game/KeyboardGame.java
+++ src/game/KeyboardGame.java
@@ -3,9 +3,10 @@ package game;
 import java.util.Scanner;
 
 public class KeyboardGame {
+	Scanner keyboard = new Scanner(System.in);
+
 	@SuppressWarnings("resource")
 	public boolean readChar(char affirmativeValue) {
-		Scanner keyboard = new Scanner(System.in);
 		char c = keyboard.next().charAt(0);
 		if (affirmativeValue == c) {
 			return true;
