23f98bd75c36fce125f2bc20b9f9146bcf2b2648
jperfor849 <jperfor849@g.educaand.es>
2022-05-03 13:37:42 +0200

    GoMoku: Human: Implement a Map for translating the letters to numbers
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>
    Signed-off-by: jperfor849 <jperfor849@g.educaand.es>

--- src/game/Human.java
+++ src/game/Human.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Human extends Player {
 	KeyboardGame keyboard = new KeyboardGame();
 
@@ -11,8 +14,12 @@ public class Human extends Player {
 	public int[] returnCoordinates(Board board) {
 		String coordinates = keyboard.readString();
 		String[] coordinates_split = coordinates.split(",");
+		Map<String, Integer> alphabet = new HashMap<>();
 		int[] positions = new int[2];
-		positions[0] = Integer.parseInt(coordinates_split[0]) - 1;
+		for (int i = 0; i < 26; i++) {
+			alphabet.put(String.valueOf((char) (i + 65)), i);
+		}
+		positions[0] = alphabet.get(coordinates_split[0]);
 		positions[1] = Integer.parseInt(coordinates_split[1]) - 1;
 		return positions;
 	}
