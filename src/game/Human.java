bcbb80329cce7e3106368854547d9f2bc024a0b5
jperfor849 <jperfor849@g.educaand.es>
2022-05-03 13:36:54 +0200

    GoMoku: Human: Use keyboardGame class
    Co-authored-by: jmpfbmx <jmpf.bmx@gmail.com>
    Signed-off-by: jperfor849 <jperfor849@g.educaand.es>

--- src/game/Human.java
+++ src/game/Human.java
@@ -1,7 +1,7 @@
 package game;
 
 public class Human extends Player {
-	KeyboardGame keyboardGame = new KeyboardGame();
+	KeyboardGame keyboard = new KeyboardGame();
 
 	public Human(String name, boolean turn) {
 		super(name, turn);
@@ -9,13 +9,11 @@ public class Human extends Player {
 
 	@Override
 	public int[] returnCoordinates(Board board) {
-		keyboardGame.keyboard.reset();
-		String coordinates = keyboardGame.keyboard.nextLine();
+		String coordinates = keyboard.readString();
 		String[] coordinates_split = coordinates.split(",");
 		int[] positions = new int[2];
 		positions[0] = Integer.parseInt(coordinates_split[0]) - 1;
 		positions[1] = Integer.parseInt(coordinates_split[1]) - 1;
-		keyboardGame.keyboard.reset();
 		return positions;
 	}
 
